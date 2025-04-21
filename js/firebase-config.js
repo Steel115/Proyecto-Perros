b317678becfdc7bed75c520c7a467a4af864def9
Steel115 <kikin.herrera28@gmail.com>
2025-04-21 17:51:26 -0600

    12

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,32 +1,17 @@
-// Escuchar cambios en Firebase
-firebase.database().ref('perros/').on('value', snapshot => {
-  const datos = snapshot.val() || {};
-  
-  Object.keys(datos).forEach(dogId => {
-    const { lat, lng } = datos[dogId].ubicacion;
-    actualizarMapa(dogId, lat, lng);
-  });
-});
+// Configuración REAL de tu proyecto Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAYWxKPTV2xnxvdent0fn7idV95ksFizX4",
+  authDomain: "perros-perdidos-59490.firebaseapp.com",
+  databaseURL: "https://perros-perdidos-59490-default-rtdb.firebaseio.com",
+  projectId: "perros-perdidos-59490",
+  storageBucket: "perros-perdidos-59490.firebasestorage.app",
+  messagingSenderId: "308422965547",
+  appId: "1:308422965547:web:f9402c6f67eaca208cad6e",
+  measurementId: "G-11Z6E11X8L"
+};
 
-// Función mejorada para actualizar mapas
-function actualizarMapa(dogId, lat, lng) {
-  const mapaDiv = document.getElementById(`map-${dogId}`);
-  
-  if (!mapaDiv) return;
-
-  // Destruir mapa existente si hay uno
-  if (mapaDiv._map) {
-    mapaDiv._map.off();
-    mapaDiv._map.remove();
-  }
-
-  // Crear nuevo mapa
-  mapaDiv._map = L.map(mapaDiv).setView([lat, lng], 15);
-  
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapaDiv._map);
-  
-  // Marcador con popup
-  mapaDiv._marker = L.marker([lat, lng]).addTo(mapaDiv._map)
-    .bindPopup(`Último reporte: ${new Date().toLocaleTimeString()}`)
-    .openPopup();
+// Inicialización segura
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
+const database = firebase.database();
