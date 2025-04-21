d465ae9430171163972e6b2791f67b28d664fd87
Steel115 <kikin.herrera28@gmail.com>
2025-04-21 16:28:05 -0600

    Mejora 2

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,12 +1,12 @@
 // Configuración de Firebase (la obtendrás en el paso 3)
 const firebaseConfig = {
-    apiKey: "AIzaSyAYWxKPTV2xnxvdent0fn7idV95ksFizX4",
-    authDomain: "perros-perdidos-59490.firebaseapp.com",
-    databaseURL: "https://perros-perdidos-59490-default-rtdb.firebaseio.com",
-    projectId: "perros-perdidos-59490",
-    storageBucket: "perros-perdidos-59490.firebasestorage.app",
-    messagingSenderId: "308422965547",
-    appId: "1:308422965547:web:f9402c6f67eaca208cad6e",
+    apiKey: process.env.FIREBASE_API_KEY || "AIzaSyAYWxKPTV2xnxvdent0fn7idV95ksFizX4",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "perros-perdidos-59490.firebaseapp.com",
+    databaseURL: process.env.FIREBASE_DATABASE_URL || "https://perros-perdidos-59490-default-rtdb.firebaseio.com",
+    projectId: process.env.FIREBASE_PROJECT_ID || "perros-perdidos-59490",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "perros-perdidos-59490.firebasestorage.app",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "308422965547",
+    appId: process.env.FIREBASE_APP_ID || "1:308422965547:web:f9402c6f67eaca208cad6e",
   };
   
   // Inicializa Firebase
